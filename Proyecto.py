0fe102a6d1e78e9942688c8aed41cd400fd1ed70
Rodrigo <rodrybarretofernandez@gmail.com>
2025-08-25 15:30:36 -0500

    Añadiendo la funcionalidad de suma en el proyecto de Python

---
+++ Proyecto.py
@@ -0,0 +1,7 @@
+# Suma de dos numeros
+print("Realizando una suma de dos numeros:")
+numero1 = input("ingrese su primer numero: ")
+numero2 = input("ingrese su segundo numero: ")
+
+# Realizando la suma
+print("suma: ", int(numero1) + int(numero2))
